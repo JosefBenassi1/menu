6ced940891302be31950d2a4643be3025a25927e
Patrick Clover <patrick@blackbx.io>
2017-06-11 23:11:06 +0100

    Gulp task change

--- gulp_tasks/styles.js
+++ gulp_tasks/styles.js
@@ -1,20 +1,41 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync');
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync');
+const sourcemaps   = require('gulp-sourcemaps');
+const sass         = require('gulp-sass');
+const postcss      = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
-
-const conf = require('../conf/gulp.conf');
+const conf         = require('../conf/gulp.conf');
+const inject       = require('gulp-inject');
 
 gulp.task('styles', styles);
 
 function styles() {
-  return gulp.src(conf.path.src('index.scss'))
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'})).on('error', conf.errorHandler('Sass'))
-    .pipe(postcss([autoprefixer()])).on('error', conf.errorHandler('Autoprefixer'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(conf.path.tmp()))
-    .pipe(browserSync.stream());
+
+    const partialsInjectFiles = gulp.src(
+        [
+            'bower_components/bootstrap-sass/assets/stylesheets/*.scss',
+            conf.path.src('/app/**/*.scss')
+        ],
+        {
+            read: false
+        });
+
+    const partialsInjectOptions = {
+        transform   : (filePath) => {
+            filePath = filePath.replace(conf.path.src, '');
+            return '@import "' + filePath + '";';
+        },
+        starttag    : '// injector',
+        endtag      : '// endinjector',
+        addRootSlash: false
+    };
+
+    return gulp.src(conf.path.src('index.scss'))
+        .pipe(inject(partialsInjectFiles, partialsInjectOptions))
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'})).on('error', conf.errorHandler('Sass'))
+        .pipe(postcss([autoprefixer()])).on('error', conf.errorHandler('Autoprefixer'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(conf.path.tmp()))
+        .pipe(browserSync.stream());
 }
