8450d701eefb52c591159e470f468bc6d7f58abd
Patrick Clover <patrick@blackbx.io>
2017-06-11 22:56:05 +0100

    Serve prod

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app     = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.get('/', (req, res) => {
-    res.sendfile('index.html', {root: __dirname});
-});
-const server = app.listen(process.env.PORT || 80);
