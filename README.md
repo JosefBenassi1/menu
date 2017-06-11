cacf04870008e318595bbd45d2bfeb4adf6b7f7f
Patrick Clover <patrick@blackbx.io>
2017-06-11 20:29:13 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Project generator - http://fountainjs.io/download
+    Angular >1.6 - frontend framework
+    Bower - frontend dependancy management
+    SASS - compiles down to css allows for programatic functions
+    Babel - Allows for modern day Javascript coding, will be compiled down to vanillaJS
+    Gulp - Frontend compiler
---
+++ gulp_tasks/deploy.js
@@ -0,0 +1,14 @@
+const gulp    = require('gulp');
+const connect = require('gulp-connect');
+const conf    = require('../conf/gulp.conf');
+
+gulp.task('serveprod', function() {
+
+
+
+    connect.server({
+        root      : conf.path.dist(),
+        port      : process.env.PORT || 5000, // localhost:5000
+        livereload: false
+    });
+});
